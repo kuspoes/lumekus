4a655393fe04945058711e79554dcf49e01b8f0a
kuspoes <kusaeni@gmail.com>
2022-06-20 11:15:50 +0700

    adding cusdis

--- server.ts
+++ server.ts
@@ -4,9 +4,9 @@ import expires from "https://deno.land/x/lume/middlewares/expires.ts";
 const s = new Server({
    port: 8080,
    root: `${Deno.cwd()}/_site`,
-})
+});
 
 s.use(expires());
 s.start();
 
-console.log(`Listening on port: ${ port } `)
+console.log(`Listening on port: ${ port } `);
